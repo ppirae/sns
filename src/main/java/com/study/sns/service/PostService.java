9a37c575802651f595f7fb31589052333dc27248
Hyeongrae Jo <76774733+ppirae@users.noreply.github.com>
2022-10-03 14:36:41 +0900

    feat: add post-modifying Test

--- src/main/java/com/study/sns/service/PostService.java
+++ src/main/java/com/study/sns/service/PostService.java
@@ -25,4 +25,13 @@ public class PostService {
                 new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
         postEntityRepository.save(PostEntity.of(title, body, userEntity));
     }
+
+    @Transactional
+    public void modify(String title, String body, String userName, Integer postId) {
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+        //post exist
+
+        //post permission
+    }
 }
