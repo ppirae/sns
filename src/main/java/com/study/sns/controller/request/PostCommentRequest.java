6e3f8c44d277f03e1f880fe7a315eccd9f65d9c9
Hyeongrae Jo <76774733+ppirae@users.noreply.github.com>
2022-10-04 19:42:21 +0900

    feat: add comment-writing controller Test

---
+++ src/main/java/com/study/sns/controller/request/PostCommentRequest.java
@@ -0,0 +1,11 @@
+package com.study.sns.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PostCommentRequest {
+
+    private String comment;
+}
